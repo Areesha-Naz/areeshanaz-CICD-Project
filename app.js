e8ea41be39003f222406da3b67db92680f55c837
Areesha <areeshanaz05@gmail.com>
2026-06-24 16:37:48 +0500

    Update message v2 - webhook test

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello from Areesha Jenkins Pipeline! Updated Version\n');
+  res.end('Hello from Areesha Jenkins Pipeline! v2 - Auto Deployed!\n');
 }).listen(3000, () => console.log('Running on port 3000'));
